01d9bba8008c99b2847f0dbaf6b700d11e1c0a6d
uy5gf <90857532+uy5gf@users.noreply.github.com>
2021-10-26 03:31:55 +0200

    Add files via upload

---
+++ javascript/castigos.js
@@ -0,0 +1,33 @@
+var num = 0;
+
+numTotal = 24; //numero total de castigos
+
+function showAll() {
+    num++;
+
+    for(i=1; i<=numTotal; i++) {
+        document.getElementById("imagen"+i).style.display = "inline-block";
+        document.getElementById("imagen"+i).style.marginLeft = "0px";
+    }
+
+    if (num%2!=0) {
+        document.getElementById("castigos").style.display = "block";
+    } else {
+        document.getElementById("castigos").style.display = "none";
+    }
+}
+
+function rollOne() {
+    res = Math.floor((Math.random() * numTotal) + 1);
+    num = 0;
+    console.log('rollOne', num);
+    for(i=1; i<=numTotal; i++) {
+        if (i!= res) {
+            document.getElementById("imagen"+i).style.display = "none";
+        } else {
+            document.getElementById("imagen"+res).style.display = "block";
+            document.getElementById("imagen"+res).style.marginLeft = "40%";
+        }
+    }
+    document.getElementById("castigos").style.display = "block";
+}
